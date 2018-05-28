122ca57e9ac360bca68904ed00768d6f35e5163c
Your Name <you@example.com>
2018-05-28 05:50:54 +0200

    spectre

--- Spectre&MEltdown/Spectre.c
+++ Spectre&MEltdown/Spectre.c
@@ -5,6 +5,7 @@
 #include <x86intrin.h>
 
 #define CACHE_PAGE 128;
+#define COUNT 1000;
 
 volatile uint8_t detector[256 * CACHE_PAGE];
 
@@ -13,7 +14,12 @@ void sink(uint_32 x){
 } 
 
 void delay(){
-    for(volatile int i = 0; i < 1000; i++){}
+    uint32_t x = 0x1234;
+    for(volatile int i = 0; i < 1000; i++){
+        x *= i;
+        x ^= 123;
+        x * 173;
+    }
 }
 
 uint32_t check(int idx){
@@ -26,11 +32,25 @@ uint32_t check(int idx){
     idx *= CACHE_PAGE;
     a = __rdtdc();
     sink(detector[idx]);
+    __mm_lfence();
     b = __rdtdc();
     return (unit32_T)(a - b);
 }
 
-int main(void){
+uint32_t check2(int idx){
+    uint32_t sum = 0;
 
+    for(int i = 0; i < COUNT; i++){
+        sum += check(idx);
+    }
+
+    return sum / COUNT;
+}
+
+int main(void){
+    uint32_t x = 0x1234;
+    for(int i = 'A'; i <= 'Z'; i++){
+        printf("/c: %u\n",i, check2(i));
+    }
     return 0;
 }
