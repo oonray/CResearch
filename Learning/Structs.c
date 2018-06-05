aca39e1053723e09887c83b322e1db15c530c03d
oonray <alexanderbjornsrud@gmail.com>
2018-06-05 15:35:50 +0200

    Struct pointer .

--- Learning/Structs.c
+++ Learning/Structs.c
@@ -14,7 +14,7 @@ struct Person *Person_create(char *name, int age, int height, int weight){
     struct Person *who = malloc(sizeof(struct Person));
     assert(who != NULL);
 
-    who->name = strdup(name);
+    (*who).name = strdup(name);
     who->age = age;
     who->height = height;
     who->weight = weight;
