898e4c22d2d76aa1e52a5c823d0a05f005ee63cc
oonray <alexanderbjornsrud@gmail.com>
2018-06-08 15:36:24 +0200

    ads

--- Learning/Memaloc.c
+++ Learning/Memaloc.c
@@ -33,3 +33,39 @@ void die(const char message){
     exit(1);
 }
 
+void Address_Print(struct Address *addr){
+    printf("%s: \n\t %s\n",addr->name,addr->email);
+}
+
+void Database_load(struct Connection *conn){
+    int rc = fread(conn->db, sizeof(struct Database),1,conn->file);
+    if(rc != 1)
+        die("Faild to load database");
+}
+
+struct Connection *database_open(const char *filename,char mode){
+    struct Connection *conn = malloc(sizeof(struct Database));
+    if(!conn)
+        die("Memmory Error!");
+
+    conn->db = malloc(sizeof(struct Database));
+    if(!conn->db)
+        die("Memmory erroor");
+    
+    if(mode == 'c'){
+        conn->file = fopen(filename, "w");
+    }else{
+        conn->file = fopen(filename,"r+");
+        if(conn->file){
+            Database_load(conn);
+        }
+    }
+
+    if(!conn->file)
+        die("Failed to open File");
+    
+    return conn;
+}
+
+
+void Database_close(){}
