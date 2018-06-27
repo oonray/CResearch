3a758331016984d05a10800080f67f53832f760a
Your Name <you@example.com>
2018-06-27 13:33:28 +0200

    Debug

--- Learning/Basics/src/Debugmacros.c
+++ Learning/Basics/src/Debugmacros.c
@@ -1,4 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
+#include "headders/debug.h"
+
+int main(int argc, char *argv[]){
+
+    return 1;
+}
---
+++ Learning/Basics/src/headders/debug.h
@@ -0,0 +1,24 @@
+#ifndef __dbg_h__
+#define __dbg_h__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#ifdef NDEBUG
+#define debug(M, ...)
+#else
+#define debug(M, ...) fprintf(stderr, "DEBUG: %s:%d " M "\n",__file__, __line__, ##__VA_ARGS__)
+#endif
+
+#define clean_errno()(errno == 0 ? "None" : stderror(errno))
+#define log_err(M, ...) fprintf(stderr, "ERROR: %s:%d errno: %s" M "\n",__file__, __line__, celean_errno(), ##__VA_ARGS__)
+#define log_warn(M, ...) fprintf(stderr, "WARNING: %s:%d errno: %s" M "\n",__file__, __line__, celean_errno(), ##__VA_ARGS__)
+#define log_info(M, ...) fprintf(stderr, "INFO: %s:%d" M "\n",__file__, __line__, ##__VA_ARGS__)
+#define check(A, M, ...)if(!(A)){log_err(m, ##__VA_ARGS__); errno=0; goto error;}
+#define sentinel(M, ...){log_err(M, ##__VA_ARGS__); errno=0; goto error; }
+#define check_mem(M, ...)check((A),"Out of Memmory!")
+#define check_debug(M, ...) if(!(A)){debug(M, ##__VA_ARGS__); errno=0; goto error;}
+
+#endif
