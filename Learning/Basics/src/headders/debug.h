5f440c6adaff3dc2b02f1a8e7543f116a2ba8929
oonray <you@example.com>
2018-10-08 15:21:00 +0200

    IOFILES done

--- Learning/Basics/src/headders/debug.h
+++ Learning/Basics/src/headders/debug.h
@@ -1,23 +1,29 @@
 #ifndef __dbg_h__
 #define __dbg_h__
+#endif
 
 #include <stdio.h>
+#include <errno.h> 
 #include <string.h>
-#include <errno.h>
 
 #ifdef NDEBUG
 #define debug(M, ...)
-#else
-#define debug(M, ...) fprintf(stderr, "[?] DEBUG: %s:%d " M "\n",__FILE__,__LINE__, ##__VA__ARGS__)
+#else 
+#define debug(M, ...) fprintf(stderr, "DEBUG %s:%d:%s: " M "\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__) 
 #endif
 
-#define clean_errno()(errno == 0 ? "None" : strerror(errno))
-#define log_err(M, ...) fprintf(stderr, "[-] ERROR: %s:%d errno: %s " M "\n",__FILE__, __LINE__, clean_errno(), ##__VA_ARGS__);
-#define log_warn(M, ...) fprintf(stderr, "[!] WARNING: %s:%d errno: %s " M "\n",__FILE__, __LINE__, clean_errno(), ##__VA_ARGS__);
-#define log_info(M, ...) fprintf(stderr, "[:] INFO: %s:%d " M "\n",__FILE__, __LINE__, ##__VA_ARGS__);
-#define check(A, M, ...) if(!(A)){log_err(m, ##__VA_ARGS__); errno=0; goto error;}
-#define sentinel(M, ...){log_err(M, ##__VA_ARGS__); errno=0; goto error; }
-#define check_mem(A, ...) check((A),"Out of Memmory!")
-#define check_debug(A,M, ...) if(!(A)){debug(M, ##__VA_ARGS__); errno=0; goto error;}
+#define clean_errno() (errno == 0 ? "None" : strerror(errno))
+ 
+#define log_err(M, ...) fprintf(stderr,"[ERROR] (%s:%d:%s: errno: %s) " M "\n", __FILE__, __LINE__, __func__, clean_errno(), ##__VA_ARGS__) 
+ 
+#define log_warn(M, ...) fprintf(stderr, "[WARN] (%s:%d:%s: errno: %s) " M "\n", __FILE__, __LINE__, __func__, clean_errno(), ##__VA_ARGS__) 
+ 
+#define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d:%s) " M "\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__) 
 
-#endif
+#define check(A, M, ...) if(!(A)) { log_err(M, ##__VA_ARGS__); errno=0; goto error; } 
+
+#define sentinel(M, ...)  { log_err(M, ##__VA_ARGS__); errno=0; goto error; } 
+
+#define check_mem(A) check((A), "Out of memory.")
+ 
+#define check_debug(A, M, ...) if(!(A)) { debug(M, ##__VA_ARGS__); errno=0; goto error; } 
