f25ae0bb77a4c63f8b6d08006690df4304b765a2
oonray <alexanderbjornsrid@gmail.com>
2019-02-01 15:51:36 +0100

    fixed make file

--- Rootkit/Linux/main.c
+++ Rootkit/Linux/main.c
@@ -5,14 +5,18 @@
 #include <linux/module.h>	/* Needed by all modules */
 #include <linux/kernel.h>	/* Needed for KERN_INFO */
 
-int init_module(void)
+static int __init mothership_init(void)
 {
 	printk(KERN_INFO "Hello world 1.\n");
-
 	return 0;
 }
 
-void cleanup_module(void)
+static void __exit mothership_exit(void)
 {
 	printk(KERN_INFO "Goodbye world 1.\n");
+	return 0;
 }
+
+
+module_init(mothership_init);
+module_exit(mothership_exit);
