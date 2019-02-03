2862e42422ce65597a23d2f479174c15120ed1dc
oonray <alexanderbjornsrid@gmail.com>
2019-02-03 16:14:36 +0100

    fixed Struct

--- Rootkit/Linux/headders/kerndebug.h
+++ Rootkit/Linux/headders/kerndebug.h
@@ -9,16 +9,16 @@
 #ifdef NDEBUG
 #define debug(M, ...)
 #else 
-#define debug(M, ...) printk(KERN_INFO "\n\t%s[.]%s DEBUG %s:%d:%s \n\t\t" M "\n",KCYN,KNRM, __FILE__, __LINE__, __func__, ##__VA_ARGS__) 
+#define debug(M, ...) printk(KERN_INFO "\n%s[.]%s DEBUG %s:%d:%s" M "\n",KCYN,KNRM, __FILE__, __LINE__, __func__, ##__VA_ARGS__) 
 #endif
 
-#define log_err(M, ...) printk(KERN_ALERT "\n\t%s[-]%s ERROR (%s:%d:%s) \n\t\t" M "\n",KRED,KNRM,__FILE__, __LINE__, __func__, ##__VA_ARGS__) 
+#define log_err(M, ...) printk(KERN_ALERT "\n%s[-]%s ERROR (%s:%d:%s)" M "\n",KRED,KNRM,__FILE__, __LINE__, __func__, ##__VA_ARGS__) 
 
-#define log_warn(M, ...) printk(KERN_ALERT "\n\t%s[!]%s WARNING (%s:%d:%s) \n\t\t" M "\n",KMAG,KNRM, __FILE__, __LINE__, __func__, ##__VA_ARGS__) 
+#define log_warn(M, ...) printk(KERN_ALERT "\n%s[!]%s WARNING (%s:%d:%s)" M "\n",KMAG,KNRM, __FILE__, __LINE__, __func__, ##__VA_ARGS__) 
 
-#define log_info(M, ...) printk(KERN_INFO "\n\t%s[.]%s INFO (%s:%d:%s) \n\t\t" M "\n",KCYN,KNRM, __FILE__, __LINE__, __func__, ##__VA_ARGS__) 
+#define log_info(M, ...) printk(KERN_INFO "\n%s[.]%s INFO (%s:%d:%s)" M "\n",KCYN,KNRM, __FILE__, __LINE__, __func__, ##__VA_ARGS__) 
 
-#define log_success(M, ...) printk(KERN_INFO "\n\t%s[+]%s Sucess (%s:%d:%s) \n\t\t" M "\n",KGRN,KNRM, __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+#define log_success(M, ...) printk(KERN_INFO "\n%s[+]%s Sucess (%s:%d:%s)" M "\n",KGRN,KNRM, __FILE__, __LINE__, __func__, ##__VA_ARGS__)
 
 /*
 #define check(A, M, ...) if(!(A)) { log_err(M, ##__VA_ARGS__); errno=0; goto error; } 
