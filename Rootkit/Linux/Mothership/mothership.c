6ed681e4b69b0e3b0f42e94aa79cd864af5eb8e1
oonray <alexanderbjornsrid@gmail.com>
2019-02-06 11:01:50 +0100

    Fixed includes

--- Rootkit/Linux/Mothership/mothership.c
+++ Rootkit/Linux/Mothership/mothership.c
@@ -15,6 +15,7 @@ This module creates filhandles for communication with the bots.
 #include <linux/init.h>
 #include <linux/fs.h>
 #include <linux/uaccess.h>
+#include <linux/device.h>
 
 /*
 Definitions 	definitions.h
@@ -70,6 +71,7 @@ static int destroy_device(struct device_out *dev){
 	Destroys an IO device that then is removed from  /dev/
 	*/
 	device_destroy(dev->_class,dev->device);
+	class_destroy(dec->class);
 	unregister_chrdev(dev->major,dev->name);
 	return 0;
 }
