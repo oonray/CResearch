0a23e7fada07e403d34ae952c5938680924153b3
oonray <alexanderbjornsrid@gmail.com>
2019-02-07 13:54:37 +0100

    Added debugging messages

--- Rootkit/Linux/Mothership/mothership.c
+++ Rootkit/Linux/Mothership/mothership.c
@@ -50,12 +50,11 @@ static int create_device(struct device_out *dev){
 
 	int add;
 	log_info("Starting Device Creation");
-    alloc_chrdev_region(&dev->device, 0, 1, dev->name);
-	log_info("Device Allocated!");
-	
-	if(&dev->device != NULL){
+
+	if(alloc_chrdev_region(&dev->device, 0, 1, dev->name)==0){
 		dev->major = MAJOR(dev->device);
 		dev->minor = MINOR(dev->device);
+		log_info("Device Allocated! with MAJOR:%s",dev->major);
 	}else{
 		log_err("Could not get Chardev Region!");
 		return -1;
