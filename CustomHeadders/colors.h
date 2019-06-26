3243389ce0561e395bc8c62f336460037066af53
oonray <oonray@oonrays-MacBook-Pro.local>
2019-06-26 15:49:34 +0200

    added unit test

--- CustomHeadders/colors.h
+++ CustomHeadders/colors.h
@@ -1,3 +1,7 @@
+#ifndef __color_h
+#define __color_h
+#endif
+
 #define KNRM  "\x1B[0m"
 #define KRED  "\x1B[31m"
 #define KGRN  "\x1B[32m"
--- Learning/Minunit/dbg.h
+++ Learning/Minunit/dbg.h
@@ -1,6 +1,9 @@
 #ifndef __dbg_h__
 #define __dbg_h__
 #endif
+#ifndef __color_h
+#include "colors.h"
+#endif
 
 #include <stdio.h>
 #include <errno.h> 
@@ -9,16 +12,16 @@
 #ifdef NDEBUG
 #define debug(M, ...)
 #else 
-#define debug(M, ...) fprintf(stderr, "DEBUG %s:%d:%s: " M "\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__) 
+#define debug(M, ...) fprintf(stderr, "%s %s:%d:%s: " M "\n",KINF, __FILE__, __LINE__, __func__, ##__VA_ARGS__) 
 #endif
 
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
  
-#define log_err(M, ...) fprintf(stderr,"[ERROR] (%s:%d:%s: errno: %s) " M "\n", __FILE__, __LINE__, __func__, clean_errno(), ##__VA_ARGS__) 
+#define log_err(M, ...) fprintf(stderr,"%s (%s:%d:%s: errno: %s) " M "\n", KWAR, __FILE__, __LINE__, __func__, clean_errno(), ##__VA_ARGS__) 
  
-#define log_warn(M, ...) fprintf(stderr, "[WARN] (%s:%d:%s: errno: %s) " M "\n", __FILE__, __LINE__, __func__, clean_errno(), ##__VA_ARGS__) 
+#define log_warn(M, ...) fprintf(stderr, "%s (%s:%d:%s: errno: %s) " M "\n",KWAR, __FILE__, __LINE__, __func__, clean_errno(), ##__VA_ARGS__) 
  
-#define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d:%s) " M "\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__) 
+#define log_info(M, ...) fprintf(stderr, "%s (%s:%d:%s) " M "\n",KINF, __FILE__, __LINE__, __func__, ##__VA_ARGS__) 
 
 #define check(A, M, ...) if(!(A)) { log_err(M, ##__VA_ARGS__); errno=0; goto error; } 
 
