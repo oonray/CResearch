d07069d65f977fa3981804e882b63372638511b0
oonray <alexanderbjornsrud@gmail.com>
2020-02-12 23:12:31 +0100

    .

--- lib/dbg.h
+++ lib/dbg.h
@@ -12,7 +12,7 @@
 #ifdef NDEBUG
 #define debug(M, ...)
 #else 
-#define debug(M, ...) fprintf(stderr, "%s %s:%d:%s: " M "\n",KINF, __FILE__, __LINE__, __func__, ##__VA_ARGS__) 
+#define debug(M, ...) fprintf(stderr, "%s (%s:%d:%s) " M "\n",KINF, __FILE__, __LINE__, __func__, ##__VA_ARGS__) 
 #endif
 
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
@@ -21,7 +21,17 @@
  
 #define log_warn(M, ...) fprintf(stderr, "%s (%s:%d:%s: errno: %s) " M "\n",KWAR, __FILE__, __LINE__, __func__, clean_errno(), ##__VA_ARGS__) 
  
-#define log_info(M, ...) fprintf(stderr, "%s (%s:%d:%s) " M "\n",KINF, __FILE__, __LINE__, __func__, ##__VA_ARGS__) 
+#define log_info(M, ...) fprintf(stdout, "%s (%s:%d:%s) " M "\n",KINF, __FILE__, __LINE__, __func__, ##__VA_ARGS__) 
+
+#define log_sucess(M, ...) fprintf(stdout, "%s (%s:%d:%s) " M "\n",KNOK, __FILE__, __LINE__, __func__, ##__VA_ARGS__) 
+
+#define test_error(M, ...) fprintf(stderr, "%s (%s:%d:%s) " M "\n",KWAR, __FILE__, __LINE__, __func__, ##__VA_ARGS__) 
+
+#define test_info(M, ...) fprintf(stderr, "%s (%s:%d:%s) " M "\n",KINF, __FILE__, __LINE__, __func__, ##__VA_ARGS__) 
+
+#define test_sucess(M, ...) fprintf(stderr, "%s (%s:%d:%s) " M "\n",KNOK, __FILE__, __LINE__, __func__, ##__VA_ARGS__) 
+
+
 
 #define check(A, M, ...) if(!(A)) { log_err(M, ##__VA_ARGS__); errno=0; goto error; } 
 
--- lib/lcthw/src/lcthw/darray.h
+++ lib/lcthw/src/lcthw/darray.h
@@ -5,10 +5,43 @@
 #include <assert.h>
 #include <lcthw/dbg.h>
 
-typedef struct DArray{
+typedef struct Darray{
+    int end;
+    int max;
+    size_t elemen_size;
+    size_t expand_rate;
+    void **contents;
+}Darray;
 
+Darray *Darray_create(size_t elemen_size, size_t initial_max);
+void Darray_destroy(Darray *array);
+void Darray_clear(Darray *array);
+int Darray_expand(Darray *array);
+int Darray_contract(Darray *array);
+int Darray_push(Darray *array,void *val);
+void *Darray_pop(Darray *array);
+void *Darrat_clear_destroy(Darray *array);
 
+#define Darray_last(A) ((A)->contents[(A)->end-1])
+#define Darray_first(A) ((A)->contents[0])
+#define Darray_end(A) ((A)->end)
+#define Darray_count(A) Darray_end(A)
+#define Darray_max(A) ((A)->max)
 
-}DArray;
+static inline void Darray_set(Darray *array,int i,void *val){
+    check(i<array->max,"attempt to set darray past max!");
+    if(i > array->end){array->end=i;}
+    array->contents[i] = val;
+error:
+    return;
+}
+
+static inline void *Darray_get(Darray *array,int i){
+    check(i<array->max, "attempt to get past max!");
+    return array->contents[i];
+error:
+    return NULL;
+}
+
 
 #endif
