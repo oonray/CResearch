8a890890c997acd572b955aea9deb8eca47c25e4
oonray <alexanderbjornsrud@gmail.com>
2020-11-04 15:17:03 +0100

    .

---
+++ Learning/C#/Sockets/Sockets/Server/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;;
+
+namespace Server
+{
+    class Server
+    {
+        private Socket sock { get; }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
